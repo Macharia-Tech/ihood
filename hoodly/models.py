107fd5055e68eb281b11277ab21ad4ac343314ab
AudreyCherrie <mitchelaudrey@gmail.com>
2020-01-18 21:57:00 +0300

    installed psycopg2

--- hoodly/models.py
+++ hoodly/models.py
@@ -10,16 +10,8 @@ class Neighbourhood(models.Model):
   class that contains Neighbourhood properties
   '''
   NEIGHBOURHOOD_CHOICES = (
-    ('South B','South B'),
     ('South C','South C'),
     ('Westlands','Westlands'),
-    ('Donholm','Donholm'),
-    ('Ruaka','Ruaka'),
-    ('Imara Daima','Imara Daima'),
-    ('Syokimau','Syokimau'),
-    ('Buruburu','Buruburu'),
-    ('Kinoo','Kinoo'),
-    ('Komarock','Komarock'),
     ('Madaraka','Madaraka'),
     ('Rongai','Rongai'),
     ('Karen','Karen'),

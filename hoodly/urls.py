bf16d46f47b2b3e8888f1c46b07c611f0512596e
AudreyCherrie <mitchelaudrey@gmail.com>
2020-01-18 21:16:59 +0300

    added urls

--- hoodly/urls.py
+++ hoodly/urls.py
@@ -1,13 +1,29 @@
 from django.conf.urls import url
 from . import views 
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.home,name='home'),
-    url(r'^post/(\d+)',views.single_post, name='post'),
-    url(r'^add/postt$', views.add_post, name='add-post'),
-    url(r'^new/profile$', views.new_profile, name='new-profile'),
-    url(r'displayprofile/(?P<user_id>\d+)$',views.display_profile,name='displayprofile'),
-    url(r'^api/profile/$', views.ProfileList.as_view()),
-    url(r'^api/post/$', views.PostList.as_view()),
+  url('^$',views.index,name = 'index'),
+  url(r'^search/',views.search_business, name='search_business'),
+  url(r'^add_hood/$', views.add_hood, name='add_hood'),
+  url(r'^join/(\d+)',views.join_hood,name='join_hood'),
+  url(r'^home/$',views.home,name = 'home'),
+  url(r'^edit_hood/(\d+)',views.edit_hood,name="edit_hood"),
+  url(r'^leave_hood/(\d+)',views.leave_hood,name = 'leave_hood'),
+  url(r'^add_business/$',views.add_business,name= 'add_business'),
+  url(r'^businesses/$',views.added_businesses,name= 'added_businesses'),
+  url(r'^edit_business/(\d+)',views.edit_business,name = 'edit_business'),
+  url(r'^profile/$',views.profile,name = 'profile'),
+  url(r'^update_profile/$',views.update_profile,name= 'update_profile'),
+  url(r'^add_post/$',views.add_post,name = 'add_post'),
+  url(r'^posts/$',views.posts,name = 'posts'),
+  url(r'^edit_post/(\d+)',views.edit_post,name = 'edit_post'),
+  url(r'searched/', views.search_results, name='search_results'),
+  url(r'^delete_hood/(\d+)',views.delete_hood,name = 'delete_hood'),
+  url(r'^delete_post/(\d+)',views.delete_post,name = 'delete_post'),
+  url(r'^delete_business/(\d+)',views.delete_business,name = 'delete_business'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT
